c86061c58e68783ec4022739c5b12e580aef0f65
mioe <mioe.boss@gmail.com>
2023-07-30 12:07:05 +0300

    add clear btn

--- src/pages/edit.vue
+++ src/pages/edit.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
 import InvoiceItem from '~/components/InvoiceItem.vue'
 
+const { t } = useI18n()
 const router = useRouter()
 const appStore = useAppStore()
+const { clearInvoiceItems } = appStore
+
+const handleClear = () => {
+	clearInvoiceItems()
+	router.replace('/')
+}
 
 onMounted(() => {
 	if (!appStore.invoiceItems.length)
@@ -18,4 +25,12 @@ onMounted(() => {
 			:item="i"
 		/>
 	</div>
+
+	<button
+		class="fixed bottom-[24px] left-[16px]"
+		@click="handleClear"
+	>
+		<div class="i-mi:square-and-pencil h-[18px] w-[18px]" />
+		<span>{{ t('clear') }}</span>
+	</button>
 </template>
